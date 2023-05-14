5714e105f8e7c7a082b135188616820316a65a93
Faterzin <85128772+Faterzin@users.noreply.github.com>
2023-05-14 00:52:14 -0300

    Update index.js

--- src/bulbasaur/js/index.js
+++ src/bulbasaur/js/index.js
@@ -2,6 +2,7 @@ const botaoAlterarTema = document.getElementById("botao-alterar-tema");
 const body = document.querySelector("body");
 const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
 
+
 botaoAlterarTema.addEventListener("click", () => {
   const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
 
