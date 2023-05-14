7de06cd72709ba297f30ab7cd90c93eb1b0ec92e
Faterzin <85128772+Faterzin@users.noreply.github.com>
2023-05-14 00:21:26 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener("click", () => {
+  const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+  body.classList.toggle("modo-escuro");
+
+  if (modoEscuroEstaAtivo) {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+  } else {
+    imagemBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+  }
+});
